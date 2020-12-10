cd4e54f609684dd0c396626c54c57adbfe738188
RichA <rich_ay@sbcglobal.net>
2020-12-09 22:03:38 -0600

    Updated 'editing commnets', verified login.

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -9,7 +9,7 @@ async function commentFormHandler(event) {
       window.location.toString().split('/').length - 1
     ];
   
-    console.log(comment_text, post_id);
+    console.log("Comment text and post_id: ", comment_text, post_id);
     if (comment_text) {
       const response = await fetch('/api/comment', {
         method: 'POST',
--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -3,8 +3,17 @@
 
 async function editFormHandler(event) {
     event.preventDefault();
+
+    // Get the updated content from the web form
+    
+  const title = document.querySelector('input[name="post-title"]').value.trim();
+  const post_data = document.querySelector('textarea[name="post_data"]').value.trim();
+
+  const id = window.location.toString().split("/")[
+             window.location.toString().split("/").length - 1
+  ];
   
-    const response = await fetch(`/api/posts/${this.id}`, {
+    const response = await fetch(`/api/posts/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
           title,
