5ca89f70221cc110bb4fd6db0d8b0449ef748697
RichA <rich_ay@sbcglobal.net>
2021-01-13 13:51:26 -0600

    changed path in server.js

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.use(session(sess));
 
 app.engine( 'handlebars', hbs.engine);
