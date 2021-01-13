ec2efedd275200f940bd9df09579ade13b72cd55
RichA <rich_ay@sbcglobal.net>
2021-01-13 16:26:28 -0600

    Updated reference to \controllers in 'server.js'.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const session        = require( 'express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const express        = require('express');
-const routes         = require('./controllers');
+const routes         = require('./controllers/');
 const sequelize      = require('./config/connection');
 const helpers        = require('./utils/helpers');
 
