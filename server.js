a2713476b9af2449cf4c85a9e3594df10ac45917
RichA <rich_ay@sbcglobal.net>
2021-01-17 13:48:02 -0600

    Changed encoding back to 'true'.

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(express.static('public'));
 app.use(session(sess));
